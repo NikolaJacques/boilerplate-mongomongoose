1c6af17a79726205d8f742e23e3c28548787c8e3
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-03-21 17:30:27 +0100

    commented out mongo code

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const absolutePath = __dirname + '/views/index.html';
 app.get('/', (req,res) => {
