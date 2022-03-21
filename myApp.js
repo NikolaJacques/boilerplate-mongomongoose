b0b903f3cb0bd871a05a6108ec4dc63c1a7ec3b4
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-03-21 18:25:12 +0100

    added second parameter again in connect

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connection successful'))
   .catch(error => console.log(error));
 
