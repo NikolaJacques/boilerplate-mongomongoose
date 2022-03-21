5e64bbe3a20df64690a05435a0ea8082f88fa49e
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-03-21 17:41:13 +0100

    added useNewUrlParser again

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // console.log(process.env.MONGO_URI);
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 const absolutePath = __dirname + '/views/index.html';
 app.get('/', (req,res) => {
