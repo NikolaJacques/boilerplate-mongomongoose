f0dd8d93ae160c199fc41757f04147caf74748b8
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-03-21 17:37:57 +0100

    removed parameters in mongodb connect call

--- myApp.js
+++ myApp.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// console.log(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI);
 
 const absolutePath = __dirname + '/views/index.html';
 app.get('/', (req,res) => {
