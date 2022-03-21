4abff0797dc393d9e4ce13aaf18b288c611dc3e9
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-03-21 17:26:11 +0100

    added code to connect display html

--- myApp.js
+++ myApp.js
@@ -1,7 +1,15 @@
+const express = require('express'); 
+const app = express();
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const absolutePath = __dirname + '/views/index.html';
+app.get('/', (req,res) => {
+    res.sendFile(absolutePath);
+});
 
 let Person;
 
