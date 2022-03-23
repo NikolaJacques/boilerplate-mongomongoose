9d2bcfc7fefc324e329042718baeb49ffbeaa72d
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-03-23 17:04:51 +0100

    query chain

--- myApp.js
+++ myApp.js
@@ -118,8 +118,15 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person
+    .find({favoriteFoods: foodToSearch})
+    .sort('name')
+    .limit(2)
+    .select('-age')
+    .exec((err, data)=>{
+      if (err) return done(err);
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
